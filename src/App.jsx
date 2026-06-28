c14d382a331ffb0c3f8b7acdd80d857db2961793
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-06-28 23:01:04 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,62 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [students, setStudents] = useState([
-    {id:1, name: "Huraira"},
-    {id:2, name: "Abdul"},
-    {id:3, name: "Haris"},
-  ])
+const questions = [
+  'Why should we use React?',
+  'What is h1 tag element?'
+]
 
-  const handler= ()=>{
-  setStudents(students.slice(1))
-  }
+function App() {
+  // const [students, setStudents] = useState([
+  //   {id:1, name: "Huraira"},
+  //   {id:2, name: "Abdul"},
+  //   {id:3, name: "Haris"},
+  // ])
+
+  // const handler= ()=>{
+  // setStudents(students.slice(1))
+  // }
+
+const  [questionNum, setQuestionNum] = useState(1) 
 
 
   return (
-   <> 
-   <button onClick={handler}>Click Me</button>
-   {
-    students.map ((element)=>{
-      return <Input element={element.name} key={element.id}/>
-    })
-   }
-   </>
+    <>
+    <button onClick={()=> setQuestionNum(questionNum==1?2:1)}>Change Question</button>
+    {
+      questionNum == 1? <QuestionBox key={1} question={questions[0]}/>:<QuestionBox key={2} question={questions[1]}/>
+    }
+    </>
+
+  //  <> 
+  //  <button onClick={handler}>Click Me</button>
+  //  {
+  //   students.map ((element)=>{
+  //     return <Input element={element.name} key={element.id}/>
+  //   })
+  //  }
+
+
+  //  </>
   )
 }
 
 export default App
 
-const Input = ({element})=>{
+// const Input = ({element})=>{
+//   return(
+//     <>
+//     <input type="text" defaultValue={element} />
+//     </>
+//   )
+// }
+
+const QuestionBox = ({question})=>{
+  const [answer,setAnswer] = useState("")
   return(
     <>
-    <input type="text" defaultValue={element} />
+    <h1>{question}</h1>
+    <input style={{background:answer?"yellow":'white',color:'black'}} type="text" onChange={(e)=>setAnswer(e.target.value)} />
     </>
   )
 }
